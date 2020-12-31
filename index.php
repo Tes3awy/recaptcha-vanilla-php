6c9c598942a4444920a39521001c60b86258dd85
Osama Abbas <osaid2512@gmail.com>
2020-12-31 22:19:21 +0200

    Update form html elements

--- index.php
+++ index.php
@@ -5,13 +5,15 @@
   $secret_key = $_ENV['SECRET_KEY'];
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" dir="ltr">
 
   <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>reCAPTCHA Validation using Plain PHP</title>
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="images/favicon.ico">
+    <title>Google reCAPTCHA Server-side Validation using Plain PHP</title>
     <!-- CSS styles -->
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style.css">
@@ -21,52 +23,67 @@
 
   <body>
     <?php
+      // Server side validation
       if (isset($_POST['submit'])) {
           // reCAPTCHA response on submitting the form
           $response = $_POST['g-recaptcha-response'];
           // remoteip param is optional
           $payload = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.$secret_key.'&response='.$response);
           // Decoding JSON response from Google. TRUE param for assoc. array
-          $res = json_decode($payload, true);
+          $res = json_decode($payload, TRUE);
           // Checking payload response
           if ($res['success'] != 1) {
               // Failure case
-              $error = 'Sorry. Missing validation!!!';
+              $error = 'Sorry! Missing reCAPTCHA validation.';
           } else {
               // Success case
-              $success = 'Thank you!';
+              $success = 'Your message is sent successfully. Thank you!';
           }
       }
     ?>
     <div class="container p-4">
-      <h1 class="text-white text-center mb-5">Google reCAPTCHA V2<span class="text-info">Server-Side</span> Validation</h1>
+      <h1 class="text-white text-center mb-5">Google reCAPTCHA V2 <span class="text-info">Server-Side</span> Validation</h1>
       <div class="row">
         <div class="col-md-6 ml-auto">
           <?php if (isset($_POST["submit"])): ?>
-          <!-- Displaying validation status -->
-          <?php if (!empty($success)): ?>
-          <!-- Success case -->
-          <div class="alert alert-success" role="alert">
-            <strong>
-              <?php echo $success; ?></strong>
-          </div>
-          <?php else: ?>
-          <!-- Failure case -->
-          <div class="alert alert-danger" role="alert">
-            <strong>
-              <?php echo $error; ?></strong>
-          </div>
-          <?php endif; ?>
+              <!-- Displaying validation status -->
+              <?php if (!empty($success)): ?>
+              <!-- Success case -->
+              <div class="alert alert-success" role="alert">
+                <strong>
+                  <?php echo $success; ?></strong>
+              </div>
+              <?php else: ?>
+              <!-- Failure case -->
+              <div class="alert alert-danger" role="alert">
+                <strong>
+                  <?php echo $error; ?></strong>
+              </div>
+              <?php endif; ?>
           <?php endif; ?>
           <!-- Form -->
           <form method="POST" role="form">
             <!-- Name Field -->
-            <input class="form-control mb-3" type="text" name="name" placeholder="Your name" required>
+            <div class="form-group">
+              <input class="form-control border-bottom-0" type="text" name="name" placeholder="Full name" required>
+            </div>
+            <!-- Email Address -->
+            <div class="form-group">
+              <input class="form-control border-bottom-0" type="email" name="email" placeholder="Email address" required>
+            </div>
+            <!-- Mobile Phone -->
+            <div class="form-group">
+              <input class="form-control border-bottom-0" type="tel" name="phone" placeholder="Mobile phone" required>
+            </div>
             <!-- Message Area -->
-            <textarea class="form-control mb-3" name="message" rows="10" placeholder="Your message" required></textarea>
+            <div class="form-group">
+              <textarea class="form-control" name="message" rows="10" placeholder="Message" required></textarea>
+            </div>
             <!-- g-recaptcha div -->
-            <div class="g-recaptcha mb-3" data-theme="dark" data-callback="captchaVerified" data-expired-callback="captchaExpired"
+            <div class="form-group text-center">
+              <div class="g-recaptcha" data-theme="dark" data-callback="captchaVerified" data-expired-callback="captchaExpired"
               data-sitekey=<?php echo $site_key; ?>></div>
+            </div>
             <!-- Submit Btn -->
             <div class="text-center">
               <input class="btn btn-lg btn-info" id="submit" type="submit" name="submit" value="Send" disabled>
@@ -88,7 +105,6 @@
       function captchaExpired() {
         window.location.reload();
       }
-
     </script>
   </body>
 
